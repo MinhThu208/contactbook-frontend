c140e49fcac25874f8ca3c87b580fbca664c2bf5
Your Name <you@example.com>
2023-07-06 15:45:38 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path:"/:pathMatch(.*)*",
+        name:"notfound",
+        component: () => import("@/views/NotFound.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+        path:"/contact/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        porp: true
+    },
+];
 
-export default router
+const router = createRouter({
+    history: createWebHistory(import.meta.export.env.BASE_URL),
+    routes,
+});
+
+export default router;
